72e08c7b3a1862da6990eb782a38c0ec9d3c448f
Naufal Athallah <muhammadnaufalathallah14@gmail.com>
2025-02-11 15:00:03 +0700

    Create LuasLingkaran.js

---
+++ PHP1JS/LuasLingkaran.js
@@ -0,0 +1,7 @@
+function hitungLuasLingkaran() {
+    let R = parseFloat(prompt("Masukkan Jari-Jari Lingkaran:"));
+    let Luas = 3.14 * R * R;
+    console.log("Luas Lingkaran =", Luas);
+}
+
+hitungLuasLingkaran();
