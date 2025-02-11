504254eca9ad41d18502a6cc1986ca25116b8bf6
Naufal Athallah <muhammadnaufalathallah14@gmail.com>
2025-02-11 14:59:07 +0700

    Create LapanganBola.js

---
+++ PHP1JS/LapanganBola.js
@@ -0,0 +1,8 @@
+function hitungLuasLapangan() {
+    let P = parseFloat(prompt("Masukkan Panjang Lapangan Bola:"));
+    let L = parseFloat(prompt("Masukkan Lebar Lapangan Bola:"));
+    let Luas = P * L;
+    console.log("Luas Lapangan Bola =", Luas);
+}
+
+hitungLuasLapangan(); 
