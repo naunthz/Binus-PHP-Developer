8af3fbb6b85ffb207780c7da4f139386d3de9c68
Naufal Athallah <muhammadnaufalathallah14@gmail.com>
2025-02-11 15:15:16 +0700

    Create PseutoJs.js

---
+++ PHP1JS/PseutoJs.js
@@ -0,0 +1,14 @@
+
+// Deklarasi variabel
+let nama = "John Doe";
+let asal = "Jakarta";
+let pekerjaan = "Programmer";
+let negara = "Indonesia";
+let hobi = "Membaca buku";
+
+// Implementasi: Cetak informasi ke konsol
+console.log("Nama: " + nama);
+console.log("Asal: " + asal);
+console.log("Pekerjaan: " + pekerjaan);
+console.log("Negara: " + negara);
+console.log("Hal yang disuka: " + hobi);
