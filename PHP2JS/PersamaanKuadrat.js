f91717bea4c7314c1d5cd73f902073ac023c627c
Naufal Athallah <muhammadnaufalathallah14@gmail.com>
2025-02-12 22:31:00 +0700

    Create PersamaanKuadrat.js

---
+++ PHP2JS/PersamaanKuadrat.js
@@ -0,0 +1,7 @@
+let a = parseFloat(prompt("Masukkan nilai A: "));
+let b = parseFloat(prompt("Masukkan nilai B: "));
+let c = parseFloat(prompt("Masukkan nilai C: "));
+
+let diskriminan = (b * b) - (4 * a * c);
+
+console.log(`Hasil Kalkulasi Diskriminan: ${diskriminan}`);
