0440822f01f74fdeea0ed1c969151247a1fcfb4a
Naufal Athallah <muhammadnaufalathallah14@gmail.com>
2025-02-12 22:28:21 +0700

    VolumeBola.js

--- PHP2JS/VolumeBola.js
+++ PHP2JS/VolumeBola.js
@@ -1,6 +1,6 @@
 let r = parseFloat(prompt("Masukin jari-jari bola (meter): "));
 
 let pi = 3.14159;
-let volume = (4 / 3) * pi * (r * r * r); // r^3 ditulis sebagai r * r * r
+let volume = (4 / 3) * pi * (r * r * r); 
 
 console.log(`Volume bola: ${volume.toFixed(2)} meter kubik`);
