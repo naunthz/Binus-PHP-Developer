13a20ada1497d41dadfe4095a7e10d1892a4e770
Nathans <muhammadnaufalathallah14@gmail.com>
2025-02-13 14:04:51 +0700

    Create GanjilGenap.js

---
+++ PHP3JS/GanjilGenap.js
@@ -0,0 +1,7 @@
+let angka = parseInt(prompt("Masukkan sebuah angka:"));
+
+if (angka % 2 === 0) {
+    console.log(`Angka ${angka} adalah angka Genap.`);
+} else {
+    console.log(`Angka ${angka} adalah angka Ganjil.`);
+}
